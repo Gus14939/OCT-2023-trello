5ae5a2e8a910840ee8844343a6d5071b71661f92
Gus <149393@coderacademy.edu.au>
2024-02-25 17:27:05 +1100

    Flask validation

--- models/card.py
+++ models/card.py
@@ -1,5 +1,11 @@
+from marshmallow import fields, validates
+from marshmallow.validate import Length, And, Regexp, OneOf
+from marshmallow.exceptions import ValidationError
+
 from init import db, ma
-from marshmallow import fields
+
+VALID_STATUSES = ('To Do', 'Ongoing', 'Done', 'Testing', 'Deployed')
+VALID_PRIORITIES = ('Low', 'Medium', 'High', 'Urgent')
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -14,7 +20,7 @@ class Card(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='cards')
-    comments = db.relationship('Comment', back_populates='card')
+    comments = db.relationship('Comment', back_populates='card', cascade='all, delete')
 
     # {id: 1, title: Card 1, user_id: 2}
     # {
@@ -27,7 +33,26 @@ class Card(db.Model):
     # }
 
 class CardSchema(ma.Schema):
-
+    
+    title = fields.String(required=True, validate=And(
+        Length(min=2, error="Title must be at least 2 chatacters long"),
+        Regexp('^[a-zA-Z0-9 ]+$', error="Title can only have alphanumeric characters")
+    ))
+    
+    status = fields.String(validate=OneOf(VALID_STATUSES))
+    
+    priority = fields.String(validate=OneOf(VALID_PRIORITIES))
+    
+    # There can only be one card with status 'Ongoing'
+    @validates('status')
+    def validate_status(self, value):
+        print(self)
+        if value == VALID_STATUSES[1]:
+            stmt = db.select(db.func.count()).select_from(Card).filter_by(status=VALID_STATUSES[1])
+            count = db.session.scalar(stmt)
+            if count > 0:
+                raise ValidationError('You already have an Ongoing card')
+    
     user = fields.Nested('UserSchema', only = ['name', 'email'])
 
     comments = fields.List(fields.Nested('CommentSchema', exclude=['card']))
